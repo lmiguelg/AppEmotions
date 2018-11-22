e8372f629ef6de74b44a12b2008a71557702e2b0
lmiguelg <lmiguelgoncalves97@hotmail.com>
2018-11-22 12:26:54 +0000

    commit 2

---
+++ AppEI/app/src/main/java/com/example/lmigu/appei/CriarPlaylistActivity.java
@@ -0,0 +1,13 @@
+package com.example.lmigu.appei;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CriarPlaylistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_playlist);
+    }
+}
--- AppEI/app/src/main/java/com/example/lmigu/appei/MainActivity.java
+++ AppEI/app/src/main/java/com/example/lmigu/appei/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.lmigu.appei;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btnCriarPlaylist;
+    Button btnOuvirMusica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Listener para o btn criar playlist
+        btnCriarPlaylist = findViewById(R.id.btn_criarPlaylist);
+        btnCriarPlaylist.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick (View v){
+
+                changeActivity(new CriarPlaylistActivity () );
+            }
+        });
+
+    }
+
+    public void changeActivity(Activity activity){//função para mudar de atividade
+        Class myActivity = activity.getClass();
+        Intent intent = new Intent(MainActivity.this, myActivity);
+        startActivity(intent);
+
     }
 }
